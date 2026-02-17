e0a33f1a05127b343621e85790ba1bffb5ec13f9
CAILLET Amelie <185417630+AmeLIE2024@users.noreply.github.com>
2026-02-17 14:37:00 +0100

    add menu option

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,7 @@ public class Main {
             System.out.println("Tour : " + tourJoueur +"\n");
             System.out.println("Vos ressources : \n" +
                             "Bois : " + ressourceJoueur.getBois() + " | Pierre : " + ressourceJoueur.getPierre() + " | Or : " + ressourceJoueur.getOr() + " | Nourriture : " + ressourceJoueur.getNourriture() + " | Habitant(s) : " + ressourceJoueur.getHabitant() );
+            optionMenu(scanner);
             scanner.nextLine();
             condition = victoryOrDefeatCondition(victoryOrDeafeat);
             tourJoueur++;
@@ -31,5 +32,13 @@ public class Main {
         }
         return false;
     }
+
+     public static void optionMenu(Scanner scanner){
+        System.out.println("\nQue souhaitez vous faire : \n\t1- Explorer la forêt: +5 bois | +3 nourritures \n\t2- Créer une mine: -10 bois \n\t3- Travailler à la mine: -5 Nourriture | +5 Pierre | +2 Or\n\t4- Recruter un habitant: -30 Or\n\t5- Commercer: -5 Pierre | +10 Or\n\t6- Construire le Château: -100 Bois | -100 Pierre | -200 Or | -40 Habitants");
+        int choiceOption = scanner.nextInt();
+
+    }
+
+    
 
 }
