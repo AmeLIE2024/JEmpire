433b1df4ea77d7db24e92eb24386c2a4f91111de
GUIBERT Vincent <vincent.guibert@externe.maif.fr>
2026-02-17 13:58:00 +0100

    feat : creation de la boucle

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
 
-    Ressource ressourceJoueur = new Ressource(0, 0, 50, 100, 1);
-    
+    public static void main(String[] args) {
+
+        Ressource ressourceJoueur = new Ressource(0, 0, 50, 100, 1);
+
+        boolean condition = false;
+        String victoryOrDeafeat = "";
+
+        while (condition) {
+            condition = victoryOrDefeatCondition(victoryOrDeafeat);
+        }
+    }
+
+    public static boolean victoryOrDefeatCondition(String victoryOrDeafeat){
+
+        if (victoryOrDeafeat.equals("deafeat")){
+            System.out.println("Loooooooooosssssserrrrrr !!!!");
+        }
+        else if (victoryOrDeafeat.equals("victory")){
+            System.out.println("Bravo vous avez créé votre chateau");
+        }
+        return true;
+    }
+
+
 }
