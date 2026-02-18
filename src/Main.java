ecbedd244d2f9892682cbe6b7c54fc16182ff912
CAILLET Amelie <185417630+AmeLIE2024@users.noreply.github.com>
2026-02-18 12:15:37 +0100

    add random

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,6 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String victoryOrDeafeat = "";
         int tourJoueur = 0;
-        
 
         System.out.println("\nBienvenue sur le jeu JEMPIRE !\n");
         while (victoryOrDefeatCondition(victoryOrDeafeat)) {
@@ -22,10 +21,11 @@ public class Main {
             if (ressourceJoueur.habitant <= 0) {
                 victoryOrDeafeat = "deafeat";
             }
-            if (ressourceJoueur.getCastle()){
+            if (ressourceJoueur.getCastle()) {
                 victoryOrDeafeat = "victory";
             }
             tourJoueur++;
+
         }
     }
 
@@ -45,12 +45,12 @@ public class Main {
         ressourceJoueur.deleteWood(10);
     }
 
-    public static void recrut(Ressource ressourceJoueur){
+    public static void recrut(Ressource ressourceJoueur) {
         ressourceJoueur.deleteGold(30);
         ressourceJoueur.addPeople(1);
     }
 
-    public static void constructCastle(Ressource ressourceJoueur){
+    public static void constructCastle(Ressource ressourceJoueur) {
         ressourceJoueur.setCastle(true);
     }
 
@@ -63,8 +63,18 @@ public class Main {
             int choice = scanner.nextInt();
             switch (choice) {
                 case 1:
-                    ressourceJoueur.addBois(5);
-                    ressourceJoueur.addNourriture(3);
+                   
+                    ressourceJoueur.addBois(5 *ressourceJoueur.getHabitant());
+                    ressourceJoueur.addNourriture(3 * ressourceJoueur.getHabitant());
+                    int aleatoire1 = (int) (Math.random() * 10);
+                    int aleatoire2 = (int) (Math.random() * 30);
+                    if ( aleatoire1 == 5) {
+                        System.out.println("Vous avez trouvé un coin à champignon !");
+                        ressourceJoueur.addNourriture(10);
+                    } else if ( aleatoire2 == 5) {
+                        System.out.println("Vou avez ramassé des champignons toxiques. Vous perdez 10 nourritures");
+                        ressourceJoueur.deleteFood(10);
+                    }
                     isValid = true;
                     break;
                 case 2:
@@ -81,44 +91,54 @@ public class Main {
                         }
                     }
                     break;
-                case 3: 
-                    if(ressourceJoueur.getMine()){
+                case 3:
+                    if (ressourceJoueur.getMine()) {
                         ressourceJoueur.deleteFood(5);
                         ressourceJoueur.addStone(5);
                         ressourceJoueur.addGold(2);
+                        int aleatoire3 =(int)(Math.random() * 20);
+                        int aleatoire4 = (int) (Math.random() * 30);
+                        if ( aleatoire3 == 10) {
+                            System.out.println("Vous avez trouvé une veine riche !");
+                            ressourceJoueur.addGold(20);
+                        } else if (aleatoire4 == 10) {
+                            System.out.println("Eboulement vous perdez 1 habitants !");
+                            ressourceJoueur.deletePeople(1);
+                        }
                         isValid = true;
-                    }else{
+                    } else {
                         System.out.println("Vous n'avez pas contruit de mine");
                     }
                     break;
-                case 4: if (ressourceJoueur.getOr() >= 30) {
-                    recrut(ressourceJoueur);
-                    isValid = true;
-                } else {
-                    System.out.println("Pas assez d'or");
-                }
+                case 4:
+                    if (ressourceJoueur.getOr() >= 30) {
+                        recrut(ressourceJoueur);
+                        isValid = true;
+                    } else {
+                        System.out.println("Pas assez d'or");
+                    }
 
                     break;
                 case 5:
-                    if(ressourceJoueur.getPierre()>=5){
+                    if (ressourceJoueur.getPierre() >= 5) {
                         ressourceJoueur.deleteStone(5);
                         ressourceJoueur.addGold(10);
                         isValid = true;
-                    }else {
+                    } else {
                         System.out.println("Ressources insuffisantes: pierres");
                     }
-                    
+
                     break;
                 case 6:
-                    if ( ressourceJoueur.getHabitant() <= 40) {
+                    if (ressourceJoueur.getHabitant() <= 40) {
                         System.out.println("Pas assez d'habitant");
-                    }else if ( ressourceJoueur.getBois() < 100){
+                    } else if (ressourceJoueur.getBois() < 100) {
                         System.out.println("Pas assez de bois");
-                    } else if ( ressourceJoueur.getOr() < 200) {
+                    } else if (ressourceJoueur.getOr() < 200) {
                         System.out.println("Pas assez d'Or");
-                    } else if ( ressourceJoueur.getPierre() < 100) {
+                    } else if (ressourceJoueur.getPierre() < 100) {
                         System.out.println("Pas assez de pierre");
-                    }else {
+                    } else {
                         constructCastle(ressourceJoueur);
                         isValid = true;
                     }
